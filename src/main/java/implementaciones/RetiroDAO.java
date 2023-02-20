de2f342263ae258ebf17d421455738d03a4b4211
Daniel Alameda <daniel.alameda228343@potros.itson.edu.mx>
2023-02-19 21:11:57 -0700

    Creación de pantallas para el retiro sin cuenta

---
+++ src/main/java/implementaciones/RetiroDAO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package implementaciones;
+
+import dominio.Retiro;
+import interfaces.IRetiroDAO;
+
+/**
+ *
+ * @author HP
+ */
+public class RetiroDAO implements IRetiroDAO{
+
+    @Override
+    public Retiro insertar(Retiro retiro) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Retiro consultar(Integer folio) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
